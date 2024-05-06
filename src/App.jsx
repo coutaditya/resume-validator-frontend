6b658e71c6787def46dd234591053435403de230
coutaditya <aditya.anand@o9solutions.com>
2024-05-06 11:42:14 +0530

    add server connection request on initial render and display alert for the same

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ButtonAppBar from './components/AppBar'
 import { Homepage } from './pages/Homepage'
 import { Query } from './pages/Query'
 import { Comparison } from './pages/Comparison'
-
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 function App() {
+  const [serverConnected, setServerConnected] = useState("info")
+  const [serverStatus, setServerStatus] = useState("Connecting to server")
+  const [displayServerStatus, setDisplayServerStatus] = useState(true)
+
+  useEffect(()=>{
+    fetch("https://dssm-project.onrender.com/")
+    .then(res=> res.json())
+    .then(data => {
+      console.log(data)
+      setServerConnected("success")
+      setServerStatus("Connected to server")
+      setTimeout(()=>{
+        setDisplayServerStatus(false)
+      }, 2000)
+    })
+    .catch(err => {
+      console.log(err)
+      setServerConnected("error")
+      setServerStatus("Could not connect to server. Try again!")
+    })
+  }, [])
+  
   return (
     <BrowserRouter>
       <ButtonAppBar />
       <div style={{ marginTop: '140px', flexGrow: 1, overflowY: 'auto' }}>
+        {(displayServerStatus) ? <Stack sx={{ width: '100%' }} spacing={2}>
+          <Alert variant="filled" severity={serverConnected}>
+            {serverStatus}
+          </Alert>
+        </Stack> : <></>}
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/resume-query" element={<Query />} />
